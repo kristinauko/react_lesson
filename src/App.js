cec8dfd1d2e31477cc3ffa4bfbe5effc8ccbd913
kristina uko <kristinaukosina@gmail.com>
2019-07-26 20:35:32 -0700

    added input field

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import './App.css';
 
 class App extends Component {
   state = {
@@ -41,11 +43,15 @@ class App extends Component {
 
     render() {
     console.log(this.state.todos)
+
     return (
       <div className="App">
-        <Header />
-        <Todos todos={this.state.todos} markComplete={this.markComplete}
-        delTodo={this.delTodo}/>
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos todos={this.state.todos} markComplete={this.markComplete}
+          delTodo={this.delTodo}/>
+        </div>
       </div>
     );
   }

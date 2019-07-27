c2899a9a6f4cb82af729adf2e5f79271ae56f650
kristina uko <kristinaukosina@gmail.com>
2019-07-26 21:30:54 -0700

    added POST request

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
-import uuid from 'uuid';
+// import uuid from 'uuid';
 import axios from 'axios';
 
 import './App.css';
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
 componentDidMount() {
-  axios.get('https://jsonplaceholder.typicode.com/todos')
+  axios.get('https://jsonplaceholder.typicode.com/todos?limit_10')
   .then(res => this.setState({todos: res.data}))
 }
 
@@ -38,12 +38,12 @@ componentDidMount() {
 
 // Add Todo
 addTodo = (title) => {
-  const newTodo = {
-    id: uuid.v4(),
-    title,
+  axios.post('https://jsonplaceholder.typicode.com/todos', {
+    title, 
     completed: false
-  }
-  this.setState({ todos: [...this.state.todos, newTodo]});
+  })
+  .then(res => this.setState({ todos: 
+    [...this.state.todos, res.data]}));
 }
     render() {
     return (
